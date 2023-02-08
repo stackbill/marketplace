e396f435c831002bc5aa5e52dd5c4268430391d3
roshanmaadhaiah <mroshan.5555@sece.ac.in>
2023-02-08 16:48:57 +0530

    changes in URL

--- lamp-20-04/lamp.sh
+++ lamp-20-04/lamp.sh
@@ -2,10 +2,10 @@
 
 mkdir -p {/usr/local/src/lamp-20-04/etc/apache2/sites-available/,/usr/local/src/lamp-20-04/var/www/html/,/usr/local/src/lamp-20-04/opt/cloudstack/,/usr/local/src/lamp-20-04/}
 
-cd /usr/local/src/lamp-20-04/etc/apache2/sites-available/ && wget https://github.com/stackbill/marketplace/raw/main/common-files/etc/apache2/sites-available/000-default.conf
+cd /usr/local/src/lamp-20-04/etc/apache2/sites-available/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/apache2/sites-available/000-default.conf
 
-cd /usr/local/src/lamp-20-04/var/www/html/ && wget https://github.com/stackbill/marketplace/raw/main/common-files/var/www/html/index.html
+cd /usr/local/src/lamp-20-04/var/www/html/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/var/www/html/index.html
 
-cd /usr/local/src/lamp-20-04/opt/cloudstack/ && wget https://github.com/stackbill/marketplace/raw/main/common-files/opt/cloudstack/lamp_cleanup.sh
+cd /usr/local/src/lamp-20-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/opt/cloudstack/lamp_cleanup.sh
 
-cd /usr/local/src/lamp-20-04/ && wget https://github.com/stackbill/marketplace/raw/main/lamp-20-04/lamp.yaml 
+cd /usr/local/src/lamp-20-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/lamp-20-04/lamp.yaml 
