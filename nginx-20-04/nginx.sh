30fad11ef3553e911221e1a3fd664b5099bbfd6f
RoshanAssistanz <roshan@assistanz.com>
2023-02-17 16:11:42 +0530

    nginx sh

--- nginx-20-04/nginx.sh
+++ nginx-20-04/nginx.sh
@@ -2,7 +2,7 @@
 
 mkdir -p {/usr/local/src/nginx-20-04/etc/nginx/sites-available/,/usr/local/src/nginx-20-04/var/www/html/,/usr/local/src/nginx-20-04/opt/cloudstack/,/usr/local/src/nginx-20-04/}
 
-cd /usr/local/src/nginx-20-04/etc/nginx/sites-available/ && https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/nginx/sites-available/Cloudstack
+cd /usr/local/src/nginx-20-04/etc/nginx/sites-available/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/nginx/sites-available/Cloudstack
 
 cd /usr/local/src/nginx-20-04/var/www/html/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/var/www/html/index.html
 
