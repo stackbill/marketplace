7f8aab1610be6414d2dcd7e86534b0a032e6cc61
RoshanAssistanz <roshan@assistanz.com>
2023-02-17 15:34:54 +0530

    wordpress fix

--- wordpress-20-04/wordpress.sh
+++ wordpress-20-04/wordpress.sh
@@ -2,18 +2,18 @@
 
 mkdir -p {/usr/local/src/wordpress-20-04/etc/apache2/sites-available/,/usr/local/src/wordpress-20-04/var/www/html/,/usr/local/src/wordpress-20-04/etc/apache2/conf-available/,/usr/local/src/wordpress-20-04/etc/fail2ban/jail.d/,/usr/local/src/wordpress-20-04/etc/apache2/conf-enabled/,/usr/local/src/wordpress-20-04/usr/share/phpmyadmin/,/usr/local/src/wordpress-20-04/opt/cloudstack/,/usr/local/src/wordpress-20-04/}
 
-cd /usr/local/src/wordpress-20-04/etc/apache2/sites-available/ && wget https://github.com/stackbill/marketplace/tree/main/common-files/etc/apache2/sites-available/001-default.conf
+cd /usr/local/src/wordpress-20-04/etc/apache2/sites-available/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/apache2/sites-available/001-default.conf
 
-cd /usr/local/src/wordpress-20-04/var/www/html/ && wget https://github.com/stackbill/marketplace/tree/main/common-files/var/www/html/index.html
+cd /usr/local/src/wordpress-20-04/var/www/html/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/var/www/html/index.html
 
-cd /usr/local/src/wordpress-20-04/etc/apache2/conf-available/ && wget https://github.com/stackbill/marketplace/tree/main/common-files/etc/apache2/conf-available/block-xmlrpc.conf
+cd /usr/local/src/wordpress-20-04/etc/apache2/conf-available/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/apache2/conf-available/block-xmlrpc.conf
 
-cd /usr/local/src/wordpress-20-04/etc/fail2ban/jail.d/ && wget https://github.com/stackbill/marketplace/tree/main/common-files/etc/fail2ban/jail.d/wordpress-cloudstack.conf
+cd /usr/local/src/wordpress-20-04/etc/fail2ban/jail.d/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/fail2ban/jail.d/wordpress-cloudstack.conf
 
-cd /usr/local/src/wordpress-20-04/etc/apache2/conf-enabled/ && wget https://github.com/stackbill/marketplace/tree/main/common-files/etc/apache2/conf-enabled/phpmyadmin.conf
+cd /usr/local/src/wordpress-20-04/etc/apache2/conf-enabled/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/apache2/conf-enabled/phpmyadmin.conf
 
-cd /usr/local/src/wordpress-20-04/usr/share/phpmyadmin/ && wget https://github.com/stackbill/marketplace/tree/main/common-files/usr/share/phpmyadmin/.htaccess
+cd /usr/local/src/wordpress-20-04/usr/share/phpmyadmin/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/usr/share/phpmyadmin/.htaccess
 
-cd /usr/local/src/wordpress-20-04/opt/cloudstack/ && wget https://github.com/stackbill/marketplace/tree/main/common-files/opt/cloudstack/wp-setup.sh
+cd /usr/local/src/wordpress-20-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/opt/cloudstack/wp-setup.sh
 
 cd /usr/local/src/wordpress-20-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/wordpress-20-04/wordpress.yaml
