594256b981bfed3a5215d88fbacec133b9ed2361
RoshanAssistanz <roshan@assistanz.com>
2023-02-17 16:34:22 +0530

    joomla-lamp-20-04

---
+++ joomla-lamp-20-04/joomla-lamp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p {/usr/local/src/joomla-lamp-20-04/etc/apache2/sites-available/,/usr/local/src/joomla-lamp-20-04/var/www/html/,/usr/local/src/joomla-lamp-20-04/etc/apache2/conf-enabled/,/usr/local/src/joomla-lamp-20-04/usr/share/phpmyadmin/,/usr/local/src/joomla-lamp-20-04/opt/cloudstack/,/usr/local/src/joomla-lamp-20-04/}
+
+cd /usr/local/src/joomla-lamp-20-04/etc/apache2/sites-available/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/apache2/sites-available/001-default.conf
+
+cd /usr/local/src/joomla-lamp-20-04/var/www/html/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/var/www/html/index.html
+
+cd /usr/local/src/joomla-lamp-20-04/etc/apache2/conf-enabled/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/apache2/conf-enabled/phpmyadmin.conf
+
+cd /usr/local/src/joomla-lamp-20-04/usr/share/phpmyadmin/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/usr/share/phpmyadmin/.htaccess
+
+cd /usr/local/src/joomla-lamp-20-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/opt/cloudstack/joomla-setup.sh
+
+cd /usr/local/src/joomla-lamp-20-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/joomla-lamp-20-04/joomla-lamp.yaml
