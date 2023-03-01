222bcb5f3acb1bf28642354e6ff60337937f8c67
RoshanAssistanz <roshan@assistanz.com>
2023-03-01 15:28:14 +0530

    prometheus-22-04

---
+++ prometheus-22-04/prometheus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p {/usr/local/src/prometheus-22-04/etc/prometheus/,/usr/local/src/prometheus-22-04/etc/systemd/system/,/usr/local/src/prometheus-22-04/opt/cloudstack/,/usr/local/src/prometheus-22-04/}
+
+cd /usr/local/src/prometheus-22-04/etc/prometheus/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/prometheus/prometheus.yaml
+
+cd /usr/local/src/prometheus-22-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/systemd/system/prometheus.service
+
+cd /usr/local/src/prometheus-22-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/etc/systemd/system/node_exporter.service
+
+cd /usr/local/src/prometheus-22-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/common-files/opt/cloudstack/cleanup.sh
+
+cd /usr/local/src/prometheus-22-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/prometheus-22-04/prometheus.yaml
