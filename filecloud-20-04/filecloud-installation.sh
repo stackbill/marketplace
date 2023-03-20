d50cd98dcdd025280e1c6ae8f4c138960cb6afb1
RoshanAssistanz <roshan@assistanz.com>
2023-03-20 14:46:47 +0530

    latest changes

---
+++ filecloud-20-04/filecloud-installation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i 's/#Match User anoncvs/ForceCommand echo Please wait until the installation is completed..../g' /etc/ssh/sshd_config && systemctl restart sshd
+
+ACCEPT_EULA=Y  apt-get install filecloud -y
+
+sed -i 's/ForceCommand echo Please wait until the installation is completed..../#Match User anoncvs/g' /etc/ssh/sshd_config && systemctl restart sshd
--- filecloud-20-04/filecloud.sh
+++ filecloud-20-04/filecloud.sh
@@ -4,4 +4,6 @@ mkdir -p {/usr/local/src/filecloud-20-04/opt/cloudstack/,/usr/local/src/fileclou
 
 cd /usr/local/src/filecloud-20-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/filecloud-cleanup.sh
 
+cd /usr/local/src/filecloud-20-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/filecloud-20-04/filecloud-installation.sh
+
 cd /usr/local/src/filecloud-20-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/filecloud-20-04/filecloud.yaml
