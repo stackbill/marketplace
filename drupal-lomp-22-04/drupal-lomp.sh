ff16a047d94f078ced71ce1a695b0f4bcb77a881
RoshanAssistanz <roshan@assistanz.com>
2023-04-01 18:57:55 +0530

    drupal-lomp-22-04

--- drupal-lomp-22-04/drupal-lomp.sh
+++ drupal-lomp-22-04/drupal-lomp.sh
@@ -6,6 +6,6 @@ cd /usr/local/src/drupal-lomp-22-04/var/www/html/ && wget https://raw.githubuser
 
 cd /usr/local/src/drupal-lomp-22-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/drupal-cleanup.sh
 
-cd /usr/local/src/drupal-lomp-22-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/drupal-22-04/composer.sh
+cd /usr/local/src/drupal-lomp-22-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/drupal-lomp-22-04/composer.sh
 
 cd /usr/local/src/drupal-lomp-22-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/drupal-lomp-22-04/drupal-lomp.yaml
