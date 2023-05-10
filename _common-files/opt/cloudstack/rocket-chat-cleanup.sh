313c8113ae35d1d7b65ea9dbf05cb1e67285f176
RoshanAssistanz <roshan@assistanz.com>
2023-05-10 12:44:47 +0530

    added rocker.chat-20-04

---
+++ _common-files/opt/cloudstack/rocket-chat-cleanup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+NC='\033[0m'
+
+echo -e "${RED}
+################################################################################################################
+#                              Your MarketPlace App has been deployed successfully!                            #
+#                                 Passwords are stored under /root/                                            #
+################################################################################################################
+${NC}"
+
+echo
+echo -e "${RED}This message will be removed in the next login!${NC}"
+echo
+echo
+echo -e "
+##################################################################################################################################################################
+Rocket.Chat is the leading open source team chat software solution. Free, unlimited and completely customizable with on-premises and SaaS cloud hosting.
+Replace email, HipChat & Slack with the ultimate team chat software solution.
+
+This Rocket.Chat image uses docker under the hood. To learn more, please read our docker documentation - https://docs.rocket.chat/deploy/prepare-for-your-deployment/rapid-deployment-methods/docker-and-docker-compose
+
+You can find the compose project in $HOME/rocketchat directory.
+
+Looking for how to use Rocket.Chat? Be sure to check our docs: https://docs.rocket.chat
+Need some help? Join our community forums https://forums.rocket.chat and https://open.rocket.chat
+##################################################################################################################################################################
+"
+echo
+echo
+echo -e "${RED}You can access the Rocker.Chat Admin UI using the below URL in your browser.${NC}"
+echo -e "${RED}http://\$IPADDRESS:3000${NC}" 
+echo
+
+
+#Cleanup script
+rm -rf /usr/local/src/
+mkdir -p /usr/local/src/
+rm -rf /var/lib/cloud/instances/*
+rm -rf /var/lib/cloud/data/*
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \; >/dev/null 2>&1
+rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
+cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+apt-get -y autoremove >/dev/null 2>&1
+apt-get -y autoclean >/dev/null 2>&1
+history -c
+cat /dev/null > /root/.bash_history
+unset HISTFILE
+
+rm -rf /root/.bashrc
+cp /etc/skel/.bashrc /root
