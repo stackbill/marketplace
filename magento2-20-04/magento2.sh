06c00c37018407bed3bf168c475ea90bb8b1c03f
RoshanAssistanz <roshan@assistanz.com>
2023-06-14 14:14:08 +0530

    ssl script for magento added

--- magento2-20-04/magento2.sh
+++ magento2-20-04/magento2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p {/usr/local/src/magento2-20-04/etc/apache2/sites-available/,/usr/local/src/magento2-20-04/etc/letsencrypt/,/usr/local/src/magento2-20-04/var/www/html/,/usr/local/src/magento2-20-04/var/www/.config/composer/,/usr/local/src/magento2-20-04/opt/cloudstack/,/usr/local/src/magento2-20-04/}
+mkdir -p {/usr/local/src/magento2-20-04/etc/apache2/sites-available/,/usr/local/src/magento2-20-04/etc/letsencrypt/,/usr/local/src/magento2-20-04/var/www/html/,/usr/local/src/magento2-20-04/var/www/.config/composer/,/usr/local/src/magento2-20-04/opt/cloudstack/,/usr/local/src/magento2-20-04/,/usr/local/src/magento2-20-04/opt/ssl_config/}
 
 cd /usr/local/src/magento2-20-04/etc/apache2/sites-available/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/etc/apache2/sites-available/magento2.conf
 
@@ -17,6 +17,8 @@ cd /usr/local/src/magento2-20-04/opt/cloudstack/ && wget https://raw.githubuserc
 cd /usr/local/src/magento2-20-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/magento2-cleanup.sh
 
 cd /usr/local/src/magento2-20-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/update_config.php
+
+cd /usr/local/src/magento2-20-04/opt/ssl_config/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/ssl_config/ssl.sh
 
 cd /usr/local/src/magento2-20-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/magento2-20-04/composer.sh
 
