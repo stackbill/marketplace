6769fafd6b5d72374c9d0252e7689d61ac595dee
RoshanAssistanz <roshan@assistanz.com>
2023-06-22 12:25:40 +0530

    zabbix-proxy-22-04

---
+++ zabbix-proxy-22-04/zabbix-proxy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p {/usr/local/src/zabbix-proxy-22-04/var/www/html/,/usr/local/src/zabbix-proxy-22-04/opt/cloudstack/,/usr/local/src/zabbix-proxy-22-04/}
+
+cd /usr/local/src/zabbix-proxy-22-04/var/www/html/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/var/www/html/index.html
+
+cd /usr/local/src/zabbix-proxy-22-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/zabbix_proxy_cleanup.sh
+
+cd /usr/local/src/zabbix-proxy-22-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/zabbix-proxy-22-04/zabbix-proxy.yaml 
