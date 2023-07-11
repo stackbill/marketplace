357b580a1a73f1e55fbcb132b6b8d302328a5b0b
RoshanAssistanz <roshan@assistanz.com>
2023-07-11 13:47:33 +0530

    apachekafka-22-04

--- apachekafka-22-04/apachekafka.sh
+++ apachekafka-22-04/apachekafka.sh
@@ -4,10 +4,10 @@ mkdir -p {/usr/local/src/apachekafka-22-04/opt/cloudstack/,/usr/local/src/apache
 
 cd /usr/local/src/apachekafka-22-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/apachekafka-cleanup.sh
 
-cd /usr/local/src/apachekafka-22-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/etc/systemd/system/zookeeper.service
+cd /usr/local/src/apachekafka-22-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/etc/systemd/system/zookeeper.service
 
-cd /usr/local/src/apachekafka-22-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/etc/systemd/system/kafka.service
+cd /usr/local/src/apachekafka-22-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/etc/systemd/system/kafka.service
 
-cd /usr/local/src/apachekafka-22-04/opt/kafka/config/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/opt/kafka/config/server.properties
+cd /usr/local/src/apachekafka-22-04/opt/kafka/config/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/kafka/config/server.properties
 
 cd /usr/local/src/apachekafka-22-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/apachekafka-22-04/apachekafka.yaml
