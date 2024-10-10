d799c0efb64e173425ccd037e8e8e3c5fdfaf932
vigneshvrm <vigneshvrm@gmail.com>
2024-10-10 16:32:44 +0530

    Added genie-acs

---
+++ genie-acs/genie-acs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p /usr/local/src/genie-acs
+
+cd /usr/local/src/genie-acs && wget https://raw.githubusercontent.com/stackbill/marketplace/main/genie-acs/genie-acs-cleanup.sh
+
+cd /usr/local/src/genie-acs && wget https://raw.githubusercontent.com/stackbill/marketplace/main/genie-acs/genie-acs.yaml
