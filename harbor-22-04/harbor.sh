dd24a6fda5e20892e016b81e6bcb648fe899a437
kanagarajb0720 <kanagaraj.b@stackbill.com>
2025-02-26 10:15:52 +0530

    Update harbor.sh

--- harbor-22-04/harbor.sh
+++ harbor-22-04/harbor.sh
@@ -2,6 +2,6 @@
 
 mkdir -p {/usr/local/src/harbor/opt/cloudstack/,/usr/local/src/harbor}
 
-cd /usr/local/src/harbor/opt/cloudstack/ && wget https://github.com/kanagarajb0720/marketplace/blob/main/harbor-22-04/cleanup.sh
+cd /usr/local/src/harbor/opt/cloudstack/ && wget -O  https://github.com/kanagarajb0720/marketplace/blob/main/harbor-22-04/cleanup.sh
 
-cd /usr/local/src/harbor  && wget https://github.com/kanagarajb0720/marketplace/blob/main/harbor-22-04/harbor-install.yml
+cd /usr/local/src/harbor && wget -O  https://github.com/kanagarajb0720/marketplace/blob/main/harbor-22-04/harbor-install.yml
