6845011db9332e021b81676a6afac37670162a80
kanagaraj <kanagaraj.b@stackbill.com>
2025-05-19 21:29:29 +0530

    test

--- nextcloud/nextcloud.sh
+++ nextcloud/nextcloud.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /usr/local/src/nextcloud
 
-cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/stackbill/marketplace/main/nextcloud/nextcloud.yaml
+cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/nextcloud/nextcloud.yaml
 cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/stackbill/marketplace/main/nextcloud/nextcloud.conf
 cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/stackbill/marketplace/main/nextcloud/nextcloud-cleanup.sh
