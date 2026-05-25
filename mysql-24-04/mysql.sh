2bfab8faaf9c8dedd7f858bad962b4bddb38461e
kanagaraj <kanagaraj.b@stackbill.com>
2026-05-25 20:57:32 +0530

    raw

--- mysql-24-04/mysql.sh
+++ mysql-24-04/mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p {/usr/local/src/mysql-24-04/opt/cloudstack/,/usr/local/src/mysql-24-04/}
-cd /usr/local/src/mysql-24-04/opt/cloudstack/ && wget -O /usr/local/src/mysql-24-04/opt/cloudstack/mysql-cleanup.sh https://github.com/kanagarajb0720/marketplace/blob/main/mysql-24-04/mysql-cleanup.sh
+cd /usr/local/src/mysql-24-04/opt/cloudstack/ && wget -O /usr/local/src/mysql-24-04/opt/cloudstack/mysql-cleanup.sh https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/mysql-24-04/mysql-cleanup.sh
 cd /usr/local/src/mysql-24-04/ && wget -O /usr/local/src/mysql-24-04/mysql-install.yaml https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/mysql-24-04/mysql-install.yaml
