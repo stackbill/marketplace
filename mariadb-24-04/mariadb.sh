9d104688068d23e1e3c81370ff6dd00d0e959e2a
kanagaraj <kanagaraj.b@stackbill.com>
2026-05-26 16:33:29 +0530

    final-sync

---
+++ mariadb-24-04/mariadb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p {/usr/local/src/mariadb-24-04/opt/cloudstack/,/usr/local/src/mariadb-24-04/}
+
+cd /usr/local/src/mariadb-24-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/mariadb-24-04/mariadb-cleanup.sh
+
+cd /usr/local/src/mariadb-24-04/ && wget https://github.com/kanagarajb0720/marketplace/blob/main/mariadb-24-04/mariadb-24-04.yaml
