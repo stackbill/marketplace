0321175f096058ca503af96931d214975cef0d50
kanagaraj <kanagaraj.b@stackbill.com>
2026-05-26 16:38:20 +0530

    yaml

--- mariadb-24-04/mariadb.sh
+++ mariadb-24-04/mariadb.sh
@@ -4,4 +4,4 @@ mkdir -p {/usr/local/src/mariadb-24-04/opt/cloudstack/,/usr/local/src/mariadb-24
 
 cd /usr/local/src/mariadb-24-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/mariadb-24-04/mariadb-cleanup.sh
 
-cd /usr/local/src/mariadb-24-04/ && wget https://github.com/kanagarajb0720/marketplace/blob/main/mariadb-24-04/mariadb-24-04.yaml
+cd /usr/local/src/mariadb-24-04/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/mariadb-24-04/mariadb-24-04.yaml
