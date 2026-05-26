241ec0d40bfda9f931935ca290379b452921d001
kanagaraj <kanagaraj.b@stackbill.com>
2026-05-26 18:01:28 +0530

    mongodb

---
+++ mongodb-24-04/cleanup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+NC='\033[0m'
+
+echo -e "${RED}
+################################################################################################################
+#                              Your MarketPlace App has been deployed successfully!                            #
+################################################################################################################
+${NC}"
+
+echo
+echo -e "${RED}This message will be removed in the next login!${NC}"
+
+#Cleanup script
+rm -rf /usr/local/src/
+mkdir -p /usr/local/src/
+rm -rf /var/lib/cloud/instances/*
+rm -rf /var/lib/cloud/data/*
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \; >/dev/null 2>&1
+rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
+cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+apt-get -y autoremove >/dev/null 2>&1
+apt-get -y autoclean >/dev/null 2>&1
+history -c
+cat /dev/null > /root/.bash_history
+unset HISTFILE
+
+rm -rf /root/.bashrc
+cp /etc/skel/.bashrc /root
+rm -rf /opt/cloudstack
