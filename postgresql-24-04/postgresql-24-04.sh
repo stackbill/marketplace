11f139cceca6f154c73fa861b6a4602c85d15449
kanagaraj <kanagaraj.b@stackbill.com>
2026-05-26 20:23:37 +0530

    script

--- postgresql-24-04/postgresql-24-04.sh
+++ postgresql-24-04/postgresql-24-04.sh
@@ -2,6 +2,6 @@
 
 mkdir -p {/usr/local/src/postgresql-24-04/opt/cloudstack/,/usr/local/src/postgresql-24-04/}
 
-cd /usr/local/src/postgresql-24-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/postgresql-24-04/cleanup.sh
+cd /usr/local/src/postgresql-24-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/postgresql-24-04/cleanup.sh
 
-cd /usr/local/src/postgresql-24-04/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/postgresql-24-04/postgresql-24-04.yaml
+cd /usr/local/src/postgresql-24-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/postgresql-24-04/postgresql-24-04.yaml
