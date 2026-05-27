53a0858bb9c90ad79a880e8416aa412c09fd60ff
kanagaraj <kanagaraj.b@stackbill.com>
2026-05-27 18:19:34 +0530

    dc

--- influxdb-24-04/influxdb.sh
+++ influxdb-24-04/influxdb.sh
@@ -2,6 +2,6 @@
 
 mkdir -p {/usr/local/src/influxdb-24-04/opt/cloudstack/,/usr/local/src/influxdb-24-04/}
 
-cd /usr/local/src/influxdb-24-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/influxdb-24-04/cleanup.sh
+cd /usr/local/src/influxdb-24-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/influxdb-24-04/cleanup.sh
 
-cd /usr/local/src/influxdb-24-04/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/influxdb-24-04/infuxdb.yaml
+cd /usr/local/src/influxdb-24-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/influxdb-24-04/infuxdb.yaml
